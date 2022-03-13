062913782ff575e93d2693d9c54e611267509919
Jon Kaplon <jon.kaplon1@gmail.com>
2022-03-13 05:40:17 -0700

    commit tree a3072700d1b5cfbc0cbe00e7e37a6dfc8a30f597

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(n1,n2)
+    return n1+n2
+end
+
+def halve (n1)
+    return nil unless n1.class == Integer
+    n1/2
+end
